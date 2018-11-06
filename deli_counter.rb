a55edb12cdde7e68e049a8ebcf5058e27cbfae9f
JC Chang <jerryjcchang@gmail.com>
2018-11-06 00:54:03 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,8 +5,8 @@ def line(line)
   if line.length == 0
     puts "The line is currently empty."
   else
-    line.each.with_index{ |name, index|
-      line_array.push("#{index}. #{name}")}
+    line.each.with_index(1) do |name, index|
+      line_array.push("#{index}. #{name}")
     end
     puts "The line is currently: #{line_array.join(" ")}"
   end
