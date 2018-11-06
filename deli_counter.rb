bcac20c1b9c0156f391ff4bdd4e5756396d6bd50
JC Chang <jerryjcchang@gmail.com>
2018-11-06 00:52:45 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,10 +2,10 @@ katz_deli = []
 
 def line(line)
   line_array = []
-  if x.length == 0
+  if line.length == 0
     puts "The line is currently empty."
   else
-    x.each.with_index(1) do |name, index|
+    line.each.with_index do |name, index|
       line_array.push("#{index}. #{name}")
     end
     puts "The line is currently: #{line_array.join(" ")}"
