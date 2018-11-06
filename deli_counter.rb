810390a80787068aa0a9cabbb7616ba14d0f1ecd
JC Chang <jerryjcchang@gmail.com>
2018-11-06 00:40:20 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,9 @@
 # Write your code here.
 def line(katz_deli, name)
   
-take_a_number(katz_deli, name)
-  katz_deli.each_with_index { |name, index| 
+def take_a_number(katz_deli, name)
+  line = []
+  line << katz_deli.each_with_index { |name, index| 
+    "Welcome #{name}. You are number #{index + 1} in line."
+  end
+  
