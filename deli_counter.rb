fb0e5c124ed6c778d5ce365ff106b955c99b4835
JC Chang <jerryjcchang@gmail.com>
2018-11-06 00:46:24 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,16 +1,5 @@
 # Write your code here.
-def line(number)
-  line_array = []
-  if number.length == 0
-    puts "The line is currently empty."
-  else
-    line_array.each.with_index(0) do |name, index|
-      line_method_array.push("#{index}. #{name}")
-    end
-    puts "The line is currently: line_array"
-  end
-end
-  
+
 # def take_a_number(katz_deli, name)
 #   line = []
 #   line << katz_deli.each_with_index { |name, index| 
